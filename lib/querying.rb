fb856e6da0b64687cdd6ea7b36a6e76709de7dd6
Tam Vu <vutam_a@hotmail.com>
2019-09-15 23:01:52 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -4,7 +4,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
     INNER JOIN series
     ON books.series_id = series.id
     WHERE series.id = 1 
-    ORDER BY books.title ASC;"
+    ORDER BY books.year ASC;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
