359842eaebd62d52c8721e0fd5db6238350a13fc
Tam Vu <vutam_a@hotmail.com>
2019-09-15 23:53:13 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,7 +30,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title
+  "SELECT DISTINCT series.title
     FROM series
     INNER JOIN characters ON series.author_id = characters.author_id
     GROUP BY characters.species = 'human'
