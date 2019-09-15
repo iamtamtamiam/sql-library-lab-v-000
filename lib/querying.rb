2bb0acc9f404e5a95358deb209d3d612f7419c89
Tam Vu <vutam_a@hotmail.com>
2019-09-15 23:18:35 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,7 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT character.species, COUNT(characters.species)
     FROM characters
-    ORDER BY COUNT(character.species) DESC LIMIT 1;"
+    ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
