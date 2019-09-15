52f3361dec215af3d85d172b3a7e040c7e36a0c9
Tam Vu <vutam_a@hotmail.com>
2019-09-15 23:51:55 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,7 +34,7 @@ def select_series_title_with_most_human_characters
     FROM series
     INNER JOIN characters ON series.author_id = characters.author_id
     GROUP BY characters.species = 'human'
-    ORDER BY COUNT(characters.species) ASC LIMIT 1;"
+    ORDER BY COUNT('human') ASC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
