69cffe8e001f45efccac9683cf0ca74a1956ef24
Tam Vu <vutam_a@hotmail.com>
2019-09-16 00:00:45 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,5 +39,8 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(character_books.books_id)
-    FROM characters"
+    FROM character_books
+    INNER JOIN characters ON character_books.character_id = characters.id
+    GROUP BY characters.name
+    ORDER BY COUNT(characters.species) DESC;"
 end
